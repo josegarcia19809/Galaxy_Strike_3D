404276c2252597981d9d61a9688da28e0e6ab039
Jose Garcia <josegarcia19809@gmail.com>
2026-01-10 14:48:29 -0600

    Galaxy Strike 3D: Player Ship Movement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -3,20 +3,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float controlSpeed = 10;
+
+    Vector2 movement;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessTranslation();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(
+            transform.localPosition.x + xOffset,
+            transform.localPosition.y + yOffset,
+            0f);
     }
 
     public void OnMove(InputValue input)
     {
-        Debug.Log(input.Get<Vector2>());
+        movement = input.Get<Vector2>();
     }
 }
