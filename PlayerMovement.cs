1db4a6937fb37b7e5fba3dc06ddcd6cf07530f3d
Jose Garcia <josegarcia19809@gmail.com>
2026-01-10 15:00:41 -0600

    Galaxy Strike 3D: Mathf.Clamp() Ship Movement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10;
+    [SerializeField] float xClampRange = 5;
+    [SerializeField] float yClampRange = 5;
 
     Vector2 movement;
 
@@ -20,11 +22,15 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(
-            transform.localPosition.x + xOffset,
-            transform.localPosition.y + yOffset,
-            0f);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 
     public void OnMove(InputValue input)
