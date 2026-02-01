6b4f6dde8da6f83be71d47f655dc4da306fdd35c
Jose Garcia <josegarcia19809@gmail.com>
2026-01-31 19:13:17 -0600

    Galaxy Strike 3D: Enemy Hit Points

--- Enemy.cs
+++ Enemy.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject destroyedFX;
+    [SerializeField] private int HitPoints = 3;
+
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        HitPoints--;
+        if (HitPoints <= 0)
+        {
+            Instantiate(destroyedFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
