0e3281ffc73aeb57e1709b5be8e5be3caac176e5
Jose Garcia <josegarcia19809@gmail.com>
2026-02-02 17:29:37 -0600

    Galaxy Strike 3D: Reload Level Using Coroutines

--- CollisionHandler.cs
+++ CollisionHandler.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private GameObject destroyedFX;
+    
+    GameSceneManager gameSceneManager;
+    
+    
     void Start()
     {
-        
+     gameSceneManager = FindFirstObjectByType<GameSceneManager>();   
     }
 
     // Update is called once per frame
@@ -17,6 +21,7 @@ public class CollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        gameSceneManager.ReloadLevel();
         Instantiate(destroyedFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
         Debug.Log(other.gameObject.name);
